7df73dd6cbccb013be5b4dba2874072f0ac3ac70
Hack Reactor Students <accounts@hackreactor.com>
2016-07-03 23:25:17 -0700

    bug fixes

--- routes.js
+++ routes.js
@@ -1,8 +1,9 @@
+var controllers = require('./controllers');
 var router = require('express').Router();
 
-router.get('/names', controller.names.get);
-router.post('/names', controller.names.post);
-router.get('/places', controller.places.get);
-router.post('/places', controller.places.post);
+router.get('/names', controllers.names.get);
+router.post('/names', controllers.names.post);
+// router.get('/places', controllers.places.get);
+// router.post('/places', controllers.places.post);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var morgan = require('morgan');
+var parser = require('body-parser');
 var request = require('request');
+var router = require('./routes');
 
 var app = express();
 
@@ -12,5 +14,11 @@ app.listen(3000, function() {
   console.log('this is our exress server');
 });
 
-request({})
+request({
+  method: 'POST',
+  uri: 'http://127.0.0.1:3000/names',
+  json: {
+    name: 'jeremy'
+  }
+});
 
